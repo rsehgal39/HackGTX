a581dc50e5b6e4eb0718982fe2f50a11454ead89
Pranavkrishna Suresh <85195581+pranavkrishnasuresh@users.noreply.github.com>
2023-10-14 20:18:03 -0400

    Add files via upload

---
+++ frontend/src/data/constants.js
@@ -0,0 +1,271 @@
+export const Bio = {
+  name: "Pranavkrishna Suresh",
+  roles: [
+    "proactive",
+    "adaptable",
+    "efficient",
+    "professional",
+    "innovative",
+    "team-oriented",
+  ],
+  description:
+    "I'm a Computer Science student at Georgia Tech (Class of 2026) with experience and research in machine learning and software development.",
+  github: "https://github.com/pranavkrishnasuresh",
+  resume: "https://drive.google.com/file/d/1JLd0IVj3y8h16vC4mXVaIJ0ZlZm8Eo1_/view?usp=sharing", //updated
+  linkedin: "https://www.linkedin.com/in/pranavkrishnasuresh/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "TailwindCSS",
+        image:
+          "https://cdn.icon-icons.com/icons2/2699/PNG/512/tailwindcss_logo_icon_167923.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Java",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      },
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      {
+        name: "C",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/1/18/C_Programming_Language.svg",
+      },
+    ],
+  },
+
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Docker",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+      },
+      {
+        name: "ROS",
+        image:
+          "https://www.vectorlogo.zone/logos/ros/ros-icon.svg",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Xcode",
+        image:
+          "https://upload.wikimedia.org/wikipedia/en/5/56/Xcode_14_icon.png",
+      },
+      {
+        name: "Latex",
+        image:
+          "https://static-00.iconduck.com/assets.00/latex-icon-2048x1568-881nd6o4.png",
+      }
+
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://www.gatech.edu/themes/custom/gatech/images/georgia-tech-logo.jpg",
+    role: "Research Intern",
+    company: "Georgia Tech Research Institute - Atlanta, Georgia",
+    date: "June 2022 - July 2022",
+    desc: "Worked as the software lead of my research project. My team developed an algorithm to automate the UR3 industrial robotic arm to move stem cell well plates in a clean room setting. We designed a custom universal well plate gripper-mechanism fitted to an arduino. Presented research at Invitational GTRI Conference and the technology is currently implemented at Georgia Tech's Marcus Nanotechnology Research Center. ",
+    skills: [
+      "Python",
+      "C++",
+      "ROS",
+      "MoveIt",
+      "SolidWorks",
+      "VS Code",
+      "Git"
+    ],
+    //doc: "https://www.scriptajournal.org/uploads/1/3/9/7/139783056/suresh___automating_robotic_alkerhlgiuheriurhngeiurm_to_move_stem_cell_wall_plates-2.pdf",
+  },
+  {
+    id: 1,
+    img: "https://seeklogo.com/images/U/university-of-cambridge-logo-E6ED593FBF-seeklogo.com.png",
+    role: "Research Intern",
+    company: "University of Cambridge - Cambridge, England (Remote)",
+    date: "Nov 2022 - Jan 2023",
+    desc: "Created a machine learning model for Sanskrit Word Segmentation using Natural Language Processing with CNN and RNN frameworks. Achieved 92% validation accuracy.",
+    skills: [
+      "Python",
+      "NLP",
+      "Deep Learning",
+      "VS Code",
+      "Git"
+    ],
+    //doc: "https://drive.google.com/file/d/1U3nyPkXapuW08n-eUBXvNPAhfVQQsZOM/view?usp=sharing",
+
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://www.gatech.edu/themes/custom/gatech/images/georgia-tech-logo.jpg",
+    school: "Georgia Institute of Technology",
+    date: "Aug 2023 - May 2026",
+    Grade: "N/A",
+    desc: "Activites and Societies: Data Science @ GT, RoboJackets, Hindu Yuva",
+    degree: "Bachelor of Science - Computer Science",
+  },
+  {
+    id: 1,
+    img: "https://uploads-ssl.webflow.com/65029a1fced1cb897f32b854/65029a1fced1cb897f32b893_fulton_logo_shield.png",
+    school: "Fulton Science Academy Private School",
+    date: "Aug 2019 - May 2023",
+    grade: "104/100",
+    desc: "Activites and Societies: FBLA (President), Hack Club (President), Wizards of Wall Street (President), Spanish National Honor Society (President), Scripta and Astronomy Society for Research (Editor-in-Chief), Science Olympiad (Team Member)",
+    degree: "High School Diploma",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "Diagnosing Alzheimer's Disease with Audio-Based Speech Analysis",
+    date: "Jan 2023 - May 2023",
+    description:
+      "Created a machine learning model to diagnose Alzheimer's disease from spoken audio samples with a spectrogram-based CNN architecture with 87% validation accuracy.",
+    image:
+      "https://miro.medium.com/v2/resize:fit:960/1*aQCn-ZwykVVtjYKi2U8mPA.gif",
+    tags: [
+      "Python",
+      "CNN",
+      "VS Code",
+      "Google Colab",
+    ],
+    category: "Machine Learning",
+    github: "https://github.com/pranavkrishnasuresh/alzheimers",
+    //webapp: "",
+  },
+
+  {
+    id: 1,
+    title: "Atlas - The College Guide",
+    date: "Nov 2022",
+    description:
+      "iOS app recognized by US Congresswoman Lucy McBath as the winner of the Congressional App Challenge. Developed to assist high school students in the college application process. Personalies information about resources and clubs based on a student's interests.",
+    image:
+      "https://static.wixstatic.com/media/a3845d_0f9f4954db31466498958691cb9c7059~mv2.png/v1/fill/w_440,h_328,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Internship%20header.png",
+    tags: [
+      "Xcode",
+      "Adobe Illustrator"
+    ],
+    category: "app",
+    github: "https://apps.apple.com/us/app/atlas-the-college-guide/id6444067812",
+    //webapp: "",
+  },
+
+  {
+    id: 2,
+    title: "Predicting Carbon Emission Levels",
+    date: "June 2021 - July 2021",
+    description:
+      "Developed a machine learning algorithm to predict global greenhouse gas emissions in Columbia University's Summer Immersion Program. Used environmental indicators from the World Bank Indicators Collection to assess the magnitude of emissions.",
+    image:
+      "https://www.danielshealth.com/sites/danielshealth.com/files/NEW%20Blog/Blog-Header-CS-GGEmissions.gif",
+    tags: ["Python", "R", "Google Colab", "Git", "LaTeX"],
+    category: "Machine Learning",
+    github: "https://drive.google.com/file/d/1o2gCB4w5qT4xRCb1U4CvwZi17qn80oBR/view?usp=sharing",
+    //webapp: "",
+  },
+
+  {
+    id: 3,
+    title: "Detecting Distracted Driving using CNN Image Classification",
+    date: "Dec 2020 - Jan 2021",
+    description:
+      "Developed a machine learning algorithm utilizing transfer learning on the VGG19 from ImageNet to classify distracted driving from data received from driver-facing cameras into 4 categories; using the radio, using the mirror, using a cup to drink, and using a smartphone.",
+    image:
+      "https://camo.githubusercontent.com/d1f806fc49b6e0df215612315fdc233538d785599f79a000aa37e5d644d4191d/68747470733a2f2f6974737468657265616c64796c2e66696c65732e776f726470726573732e636f6d2f323031362f30362f6f75747075745f646562386f742e6769663f773d31303030",
+    tags: [
+      "Python",
+      "VS Code",
+      "Git",
+    ],
+    category: "Machine Learning",
+    github: "https://drive.google.com/file/d/1eacXz69eTMZibnFOWNjYbSwL62p_YZ4d/view?usp=sharing",
+    //webapp: "",
+  },
+
+ {
+    id: 4,
+    title: "Diagnosing Viral and Bacterial Pneumonia using CNN Image Classification",
+    date: "Oct 2022 - Present",
+    description:
+      "Developed a machine learning algorithm utilizing CNN architecture to classify viral and bacterial Pneumonia using high-specificity chest x-rays. Achieved a 91% validation accuracy, improvement over previous systems.",
+    image:
+      "https://prod-images-static.radiopaedia.org/images/1371188/0a1f5edc85aa58d5780928cb39b08659c1fc4d6d7c7dce2f8db1d63c7c737234_gallery.jpeg",
+    tags: [
+      "Python",
+      "VS Code",
+      "Git",
+    ],
+    category: "Machine Learning",
+    github: "https://drive.google.com/file/d/1oBq_d0_8JhPpZqPHAmcRqJ9-U-hN0x03/view?usp=sharing",
+    //webapp: "",
+  },
+];
